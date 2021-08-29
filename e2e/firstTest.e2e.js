18f0ba8569fcfc937379a5ddadaa19074aecebfd
Lineu <54123248+lineuxyz@users.noreply.github.com>
2021-08-29 11:27:26 -0300

    create exemple testing

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,18 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  it('check register a new skill', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+  
+});
